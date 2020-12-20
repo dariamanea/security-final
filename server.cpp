726515a7000447bce3dcbfbdaa7de72e9578685e
Jacob J <46195775+JJ2990@users.noreply.github.com>
2020-12-19 23:18:27 -0500

    Updated server.cpp with info from server.c

--- server.cpp
+++ server.cpp
@@ -1,3 +1,15 @@
+/*
+Program:
+launchServer: creates socket for listening using TLS
+checkPassword: given hashed password and username, checks if it matches the stored hashed password for that user.
+recv: server should be able to receive the public key from the user and then store it if it’s a new one. Creates a certificate for user, stores on server and returns it
+changePassword: given hashed password and username, deletes stored hashed password for user and adds new one
+sendRcptCert: send should be able to check if the users’ certificate is valid or correct and send the request document (recipient certificate) back
+sendMsg: send should be able to check if the users’ certificate is valid or correct and send the request document (message) back
+storeMsg: store message in respective recipient mailbox on server side
+check: check if the request it’s valid (ex: if there are any messages pending for that user, we should reject)
+*/
+
 #include <memory>
 #include <signal.h>
 #include <stdexcept>
@@ -11,6 +23,29 @@
 #include <openssl/bio.h>
 #include <openssl/err.h>
 #include <openssl/ssl.h>
+
+#define MAXPW 32
+
+// #define DEBUG 1
+
+#ifdef DEBUG
+#define PRINTDBG printf
+#else
+#define PRINTDBG(...)
+#endif
+
+
+int checkPassword (char *username, char *hashed_password){
+// given hashed password and username, checks if it
+// matches the stored hashed password for that user.
+
+}
+
+int changePassword (){
+    // given hashed password and username, 
+    // deletes stored hashed password for user and adds new one
+ 
+}
 
 namespace my {
 
