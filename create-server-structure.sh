efd1ad594ea6466488d44215cf94c93b7b4b41b1
Daria Manea <44726789+dariamanea@users.noreply.github.com>
2020-12-29 21:01:45 -0500

    Update create-server-structure.sh

--- create-server-structure.sh
+++ create-server-structure.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd Server
-mkdir bin users
+mkdir users
 cd users
 
 input=("addleness" "analects" "annalistic" "anthropomorphologically" "blepharosphincterectomy" "corector" "durwaun" "dysphasia" "encampment" "endoscopic" "exilic" "forfend" "gorbellied" "gushiness" "muermo" "neckar" "outmate" "outroll" "overrich" "philosophicotheological" "pockwood" "polypose" "refluxed" "reinsure" "repine" "scerne" "starshine" "unauthoritativeness" "unminced" "unrosed" "untranquil" "urushinic" "vegetocarbonaceous" "wamara" "whaledom")
