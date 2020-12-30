fd94836b61e4701a3c43ce11e4194f208bf06926
Jacob J <46195775+JJ2990@users.noreply.github.com>
2020-12-29 22:00:28 -0500

    removed sudo

--- test.sh
+++ test.sh
@@ -9,13 +9,13 @@
 cd Client/bin 
 
 # This will create certificates for user wamara and polypose
-sudo ./getcert wamara
-sudo ./getcert polypose 
+./getcert wamara
+./getcert polypose 
 
 #this will send the encrypted and signed message that is in input.txt from polypose to wamara 
 # this will store the encrypted and signed message from input.txt in Server/users/wamara/mailbox
-sudo ./sendmsg polypose wamara input.txt
+./sendmsg polypose wamara input.txt
 
 #To check message, you can run ./rcvmsg and input the username polypose and password ("lure_leagued") 
 #or wamara ("stirrer_hewer's"). 
-sudo ./recvmsg
+./recvmsg
